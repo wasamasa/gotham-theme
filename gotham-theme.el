82a0a75a70e298017df6ad1bc17ac47714293aee
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-11-03 10:54:19 +0100

    Implement makefile

--- gotham-theme.el
+++ gotham-theme.el
@@ -171,6 +171,9 @@ depending on DISPLAY for keys which are either :foreground or
    (custom-button-mouse :foreground base5 :box t)
    (custom-group-tag :inherit fixed-pitch :foreground magenta)
 
+   ;; makefile
+   (makefile-space :background magenta)
+
    ;; show-paren-mode
    (show-paren-match :foreground base2 :background cyan :weight normal :inverse-video unspecified)
    (show-paren-mismatch :foreground base2 :background red :weight normal :inverse-video unspecified)
