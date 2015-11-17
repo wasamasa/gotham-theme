d41b0ea37ad5a4d0060ea05e25d80581f113b662
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-11-17 10:58:16 +0100

    Bump version

--- gotham-theme.el
+++ gotham-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/gotham-theme
-;; Version: 1.1.4
+;; Version: 1.1.5
 
 ;; This file is NOT part of GNU Emacs.
 
