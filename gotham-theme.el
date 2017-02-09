653acfd0f68efcf492291b4c37d1ad4653f1d66b
nullx002 <nullx002@users.noreply.github.com>
2017-02-09 17:01:26 +0530

    w3m-browser support added missing face definitions

--- gotham-theme.el
+++ gotham-theme.el
@@ -894,14 +894,18 @@ depending on DISPLAY for keys which are either :foreground or
    ;; volatile-highlights
    (vhl/default-face :inherit highlight)
 
-;; w3m
-   (w3m-anchor  :inherit link)
-   (w3m-arrived-anchor :inherit link-visited)
-   (w3m-form :background base3 :foreground base0)
-   (w3m-header-line-location-title
-    :background base2 :foreground yellow)
-   (w3m-header-line-location-content
-    :background base2 :foreground base0)
+   ;; w3m
+   (w3m-anchor  :inherit link) ;; comment out if conflict with gnus
+   (w3m-arrived-anchor :inherit link-visited) ;; comment out if conflict with gnus
+   (w3m-form :background base1 :foreground magenta)
+   (w3m-form-button :background base1 :foreground cyan)
+   (w3m-form-button-mouse :background base1 :foreground green)
+   (w3m-form-button-pressed :background base1 :foreground green)
+   (w3m-form-inactive :background base1 :foreground base6)
+   (w3m-insert :background base2  :foreground magenta)
+   (w3m-error :background base1  :foreground red)     
+   (w3m-header-line-location-title :background base2 :foreground yellow)
+   (w3m-header-line-location-content :background base2 :foreground base0)
    (w3m-bold :foreground base1 :weight bold)
    (w3m-image-anchor :background base3 :foreground cyan :inherit link)
    (w3m-image :background base3 :foreground cyan)
@@ -911,16 +915,12 @@ depending on DISPLAY for keys which are either :foreground or
    (w3m-session-select :foreground base0)
    (w3m-session-selected :foreground base1 :bold t :underline t)
    (w3m-tab-background :background base3 :foreground base0)
-   (w3m-tab-selected-background
-    :background base3 :foreground base0)
+   (w3m-tab-selected-background :background base3 :foreground base0)
    (w3m-tab-mouse :background base2 :foreground yellow)
-   (w3m-tab-selected :background base2 :foreground yellow
-                                           :bold t)
+   (w3m-tab-selected :background base2 :foreground yellow :bold t)
    (w3m-tab-unselected :background base2 :foreground violet)
    (w3m-tab-selected-retrieving :background base2 :foreground red)
-   (w3m-tab-unselected-retrieving
-    :background base2 :foreground orange)
-   (w3m-tab-unselected-unseen :background base2 :foreground violet)
+   (w3m-tab-unselected-retrieving :background base2 :foreground orange)
    
    ;; web-mode
    (web-mode-block-attr-name-face :foreground green)
